355e8c76a9e54d3539943aa53bdb097e57f34f7c
Mike Wilson <geekinutah@gmail.com>
2018-09-12 16:42:31 -0600

    Change entrypoint to start apache2 in fg

--- start_keystone.sh
+++ start_keystone.sh
@@ -5,4 +5,7 @@ cat /etc/keystone/keystone.conf.j2 | python -c 'import os;import sys; import jin
 
 cat /openrc.j2 | python -c 'import os;import sys; import jinja2; sys.stdout.write(jinja2.Template(sys.stdin.read()).render(env=os.environ))' > /openrc
 
-exec /usr/local/bin/keystone-all
+echo "ServerName $HOST_ADDRESS" >> /etc/apache2/apache2.conf
+
+a2ensite keystone
+apache2ctl -D FOREGROUND
